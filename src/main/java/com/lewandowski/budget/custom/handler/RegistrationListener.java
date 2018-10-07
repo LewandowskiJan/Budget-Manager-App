44e6797b07a3e813accc845a377c98edf0896b5b
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-07 15:23:03 +0200

    RegistrationListener - methods - confirm registration

---
+++ src/main/java/com/lewandowski/budget/custom/handler/RegistrationListener.java
@@ -0,0 +1,49 @@
+package com.lewandowski.budget.custom.handler;
+
+import com.lewandowski.budget.model.User;
+import com.lewandowski.budget.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@Component
+public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
+
+    @Autowired
+    private IUserService service;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void onApplicationEvent(OnRegistrationCompleteEvent event) {
+        this.confirmRegistration(event);
+    }
+
+    private void confirmRegistration(OnRegistrationCompleteEvent event) {
+        User user = event.getUser();
+        String token = UUID.randomUUID().toString();
+        service.createVerificationToken(user, token);
+
+        String recipientAddress = user.getEmail();
+        String subject = "Registration Confirmation";
+        String confirmationUrl
+                = event.getAppUrl() + "/regitrationConfirm.html?token=" + token;
+        String message = messageSource.getMessage("message.regSucc", null, event.getLocale());
+
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(recipientAddress);
+        email.setSubject(subject);
+        email.setText(message + " rn" + "http://localhost:8080" + confirmationUrl);
+        mailSender.send(email);
+    }
+}
