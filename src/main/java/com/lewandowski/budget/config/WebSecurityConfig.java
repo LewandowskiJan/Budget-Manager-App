95075e5a22a0c978fa5cb28c36ceab89fa4a3e87
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-07 15:17:08 +0200

    WebSecurityConfig - add configuration

--- src/main/java/com/lewandowski/budget/config/WebSecurityConfig.java
+++ src/main/java/com/lewandowski/budget/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.lewandowski.budget.config;
 
+import com.lewandowski.budget.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,15 +16,23 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
     @Qualifier("dataSource")
     @Autowired
     private DataSource dataSource;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin", "/h2_console/**").permitAll();
+                .antMatchers("/**", "/admin", "/h2_console/**").permitAll();
 
 
         http.exceptionHandling().accessDeniedPage("/403");
@@ -33,9 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .maximumSessions(2)
                 .expiredUrl("/sessionExpired.html");
-
-
     }
+
 
 
 }
