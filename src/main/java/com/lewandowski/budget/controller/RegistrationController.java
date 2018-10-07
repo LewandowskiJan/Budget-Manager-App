7d362cab113469ef12bf5c5800dd5865960349a1
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-07 15:19:29 +0200

    RegistrationController - add request mapping

--- src/main/java/com/lewandowski/budget/controller/RegistrationController.java
+++ src/main/java/com/lewandowski/budget/controller/RegistrationController.java
@@ -1,10 +1,14 @@
 package com.lewandowski.budget.controller;
 
 import com.lewandowski.budget.custom.exception.EmailExistsException;
+import com.lewandowski.budget.custom.handler.OnRegistrationCompleteEvent;
 import com.lewandowski.budget.dto.UserDto;
 import com.lewandowski.budget.model.User;
+import com.lewandowski.budget.model.VerificationToken;
 import com.lewandowski.budget.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,16 +16,26 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.Calendar;
+import java.util.Locale;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
     UserService service;
+
+    @Autowired
+    ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
     public String showRegistrationForm(WebRequest request, Model model) {
@@ -37,21 +51,53 @@ public class RegistrationController {
             WebRequest request,
             Errors errors) {
 
-        User registered = new User();
-        if (!result.hasErrors()) {
-            registered = createUserAccount(accountDto, result);
-        }
-        if (registered == null) {
-            result.rejectValue("email", "message.regError");
-        }
         if (result.hasErrors()) {
             return new ModelAndView("registration", "user", accountDto);
         }
-        else {
-            return new ModelAndView("successRegister", "user", accountDto);
+
+        User registered = createUserAccount(accountDto);
+        if (registered == null) {
+            result.rejectValue("email", "message.regError");
         }
+        try {
+            String appUrl = request.getContextPath();
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent
+                    (registered, request.getLocale(), appUrl));
+        } catch (Exception me) {
+            return new ModelAndView("emailError", "user", accountDto);
+        }
+        return new ModelAndView("successRegister", "user", accountDto);
     }
-    private User createUserAccount(UserDto accountDto, BindingResult result) {
+
+
+    @RequestMapping(value = "/registrationConfirm", method = RequestMethod.GET)
+    public String confirmRegistration
+            (WebRequest request, Model model, @RequestParam("token") String token) {
+
+        Locale locale = request.getLocale();
+
+        VerificationToken verificationToken = service.getVerificationToken(token);
+        if (verificationToken == null) {
+            String message = messageSource.getMessage("auth.message.invalidToken", null, locale);
+            model.addAttribute("message", message);
+            return "redirect:/badUser.html?lang=" + locale.getLanguage();
+        }
+
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+        if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+            String messageValue = messageSource.getMessage("auth.message.expired", null, locale);
+            model.addAttribute("message", messageValue);
+            return "redirect:/badUser.html?lang=" + locale.getLanguage();
+        }
+
+        user.setEnabled(true);
+        service.saveRegisteredUser(user);
+        return "redirect:/login.html?lang=" + request.getLocale().getLanguage();
+    }
+
+
+    private User createUserAccount(UserDto accountDto) {
         User registered = null;
         try {
             registered = service.registerNewUserAccount(accountDto);
