3c4e650e880f29db019247d58d764f3e82b22b35
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-09 21:41:19 +0200

    www.baeldung.com /registration example - WebSecurityConfig - add new configuration

--- src/main/java/com/lewandowski/budget/config/WebSecurityConfig.java
+++ src/main/java/com/lewandowski/budget/config/WebSecurityConfig.java
@@ -3,12 +3,18 @@ package com.lewandowski.budget.config;
 import com.lewandowski.budget.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -23,16 +29,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**", "/admin", "/h2_console/**").permitAll();
+                .antMatchers("/**", "/admin", "/h2_console/**").permitAll()
+                .antMatchers("/user/updatePassword*",
+                        "/user/savePassword*",
+                        "/updatePassword*")
+                .hasAuthority("CHANGE_PASSWORD_PRIVILEGE");
 
 
         http.exceptionHandling().accessDeniedPage("/403");
@@ -44,7 +73,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .maximumSessions(2)
                 .expiredUrl("/sessionExpired.html");
     }
-
 
 
 }
