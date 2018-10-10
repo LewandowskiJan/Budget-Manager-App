c73b594262ceb4dd436b380303deb6aa97a76aa3
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-10 21:12:14 +0200

    www.baeldung.com /registration example - UserService refactor + new methods implemented

--- src/main/java/com/lewandowski/budget/service/IUserService.java
+++ src/main/java/com/lewandowski/budget/service/IUserService.java
@@ -1,19 +1,51 @@
 package com.lewandowski.budget.service;
 
-import com.lewandowski.budget.custom.exception.EmailExistsException;
+import com.lewandowski.budget.custom.exception.UserAlreadyExistException;
 import com.lewandowski.budget.dto.UserDto;
-import com.lewandowski.budget.model.User;
-import com.lewandowski.budget.model.VerificationToken;
+import com.lewandowski.budget.persistence.model.PasswordResetToken;
+import com.lewandowski.budget.persistence.model.User;
+import com.lewandowski.budget.persistence.model.VerificationToken;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Optional;
 
 public interface IUserService {
-    User registerNewUserAccount(UserDto accountDto)
-            throws EmailExistsException;
 
-    void createVerificationToken(User user, String token);
+    User registerNewUserAccount(UserDto accountDto) throws UserAlreadyExistException;
 
     User getUser(String verificationToken);
 
     void saveRegisteredUser(User user);
 
+    void deleteUser(User user);
+
+    void createVerificationTokenForUser(User user, String token);
+
     VerificationToken getVerificationToken(String VerificationToken);
+
+    VerificationToken generateNewVerificationToken(String token);
+
+    void createPasswordResetTokenForUser(User user, String token);
+
+    User findUserByEmail(String email);
+
+    PasswordResetToken getPasswordResetToken(String token);
+
+    User getUserByPasswordResetToken(String token);
+
+    Optional<User> getUserByID(long id);
+
+    void changeUserPassword(User user, String password);
+
+    boolean checkIfValidOldPassword(User user, String password);
+
+    String validateVerificationToken(String token);
+
+    String generateQRUrl(User user) throws UnsupportedEncodingException;
+
+    User updateUser2FA(boolean use2FA);
+
+    List<String> getUsersFromSessionRegistry();
+
 }
