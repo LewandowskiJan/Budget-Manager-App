e10edc92427ddc6f048a04257ded76baacea0afd
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-12 11:16:14 +0200

    www.baeldung.com /registration example - Captcha - service with implementation

---
+++ src/main/java/com/lewandowski/budget/captcha/ICaptchaService.java
@@ -0,0 +1,11 @@
+package com.lewandowski.budget.captcha;
+
+import com.lewandowski.budget.web.error.ReCaptchaInvalidException;
+
+public interface ICaptchaService {
+    void processResponse(final String response) throws ReCaptchaInvalidException;
+
+    String getReCaptchaSite();
+
+    String getReCaptchaSecret();
+}
