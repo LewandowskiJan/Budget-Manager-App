94c55cf1609ad06bec365af4a88d879f4b7eecaf
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-10-13 19:16:37 +0200

    www.baeldung.com /registration example - AppConfig - refactor

---
+++ src/main/java/com/lewandowski/budget/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.lewandowski.budget.config;
+
+
+import com.lewandowski.budget.security.ActiveUserStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // beans
+
+    @Bean
+    public ActiveUserStore activeUserStore() {
+        return new ActiveUserStore();
+    }
+
+}
